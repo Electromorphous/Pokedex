7445a0b247a8fa4717d913fbe7ef088ffed7faa0
Electromorphous <pstation.474@gmail.com>
2020-10-08 22:19:38 +0530

    deploy app on github

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
-import './style/main.css'
+import './CSS/main.css'
 
 ReactDOM.render(
   <React.StrictMode>
