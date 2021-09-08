2c7619679da5534c6c6d04d265095d9a0b64f240
Electromorphous <pstation.474@gmail.com>
2021-09-08 22:00:57 +0530

    final adjustments

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,57 +1,73 @@
-import React, { useEffect, useState } from "react";
-import { Grid } from "@material-ui/core";
 import React, { useEffect, useState, useMemo } from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, CircularProgress } from "@material-ui/core";
 import PokemonCard from "./PokemonCard.jsx";
-import { CircularProgress } from "@material-ui/core";
+import * as Vibrant from "node-vibrant";
 
-export default function Content({ inputText }) {
+function usePokemons() {
   const [pokemons, setPokemons] = useState([]);
 
   async function fetchData() {
-    const res = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=649"); // max limit = 649
-    res.json().then((res) => {
-      const pok = res.results.map((pokemon) => {
+    const data = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=648"); // max limit = 649
+    const res = await data.json();
+
+    return await Promise.all(
+      res.results.map(async (pokemon) => {
         const { url } = pokemon;
         const id = url.substring(34, url.length - 1);
+        let v = new Vibrant(
+          `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`
+        );
+        const palette = await v.getPalette();
 
         return {
           ...pokemon,
-          background: `rgba(${palette.Vibrant._rgb[0]}, ${palette.Vibrant._rgb[1]}, ${palette.Vibrant._rgb[2]}, 0.5)`,
+          background: `rgba(${palette.Vibrant._rgb[0]}, ${palette.Vibrant._rgb[1]}, ${palette.Vibrant._rgb[2]}, 0.6)`,
           id,
         };
-      });
-
-      setPokemons(pok);
-    });
+      })
+    );
   }
 
   useEffect(() => {
-    fetchData();
+    fetchData().then(setPokemons);
   }, []);
 
-  const filteredPokemons = pokemons.filter((pokemon) =>
-    pokemon.name.includes(inputText.toLowerCase())
-  );
+  return useMemo(() => {
+    return { pokemons };
+  }, [pokemons]);
+}
+
+export default function Content({ inputText }) {
+  const { pokemons } = usePokemons();
 
   return (
-    <Grid container>
-      {/* left padding */}
-      <Grid item xs={1} />
+    <>
+      {/* <CircularProgress color="secondary" /> */}
 
-      {/* content */}
-      <Grid item container xs={10} spacing={3} justifyContent="center">
-        {pokemons.map((pokemon) =>
-          !!pokemon.name.includes(inputText.toLowerCase()) ? (
-            <PokemonCard key={pokemon.id} pokemon={pokemon} />
-          ) : (
-            false
-          )
-        )}
-      </Grid>
+      {pokemons.length === 0 ? (
+        <CircularProgress color="secondary" />
+      ) : (
+        <Grid container>
+          {/* left padding */}
+          <Grid item xs={1} />
 
-      {/* right padding */}
-      <Grid item xs={1} />
-    </Grid>
+          {/* content */}
+          <Grid item container xs={10} spacing={3} justifyContent="center">
+            {pokemons.map((pokemon) =>
+              !!pokemon.name.includes(inputText.toLowerCase()) ? (
+                <PokemonCard key={pokemon.id} pokemon={pokemon}>
+                  {/* {console.log(5)} */}
+                </PokemonCard>
+              ) : (
+                false
+              )
+            )}
+          </Grid>
+
+          {/* right padding */}
+          <Grid item xs={1} />
+        </Grid>
+      )}
+    </>
   );
 }
